e4a12c68b6caf61c8ffd8b06c14e1590050384d9
lengyelluk <lengyel.luk@gmail.com>
2019-09-03 09:10:14 +0200

    password added to the create user in user controller

---
+++ P04-eCommerce Application/src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.demo.controllers;
+
+import com.example.demo.TestUtils;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserControllerTest {
+
+    private UserController userController;
+
+    private UserRepository userRepoMock = mock(UserRepository.class);
+
+    private CartRepository cartRepoMock = mock(CartRepository.class);
+
+    private BCryptPasswordEncoder encoder = mock(BCryptPasswordEncoder.class);
+
+    @Before
+    public void setUp() {
+        userController = new UserController();
+        TestUtils.injectObjects(userController, "userRepository", userRepoMock);
+        TestUtils.injectObjects(userController, "cartRepository", cartRepoMock);
+        TestUtils.injectObjects(userController, "bCryptPasswordEncoder", encoder);
+
+    }
+
+    @Test
+    public void create_user_happy_path() throws Exception {
+        when(encoder.encode("testtest")).thenReturn("thisIsHashed");
+        CreateUserRequest request = new CreateUserRequest();
+        request.setUsername("test");
+        request.setPassword("testPassword");
+        request.setConfirmPassword("testPassword");
+
+        final ResponseEntity<User> response = userController.createUser(request);
+
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        User user = response.getBody();
+        assertNotNull(user);
+        assertEquals(0, user.getId());
+        assertEquals("test", user.getUsername());
+        assertEquals("testPassword", user.getPassword());
+    }
+
+}
